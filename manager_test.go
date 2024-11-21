a7da8b3674183f839dfed02aeb4d9ff5093a4aa9
hhf <3531095171@qq.com>
2024-11-21 12:16:34 +0800

    update

--- manager_test.go
+++ manager_test.go
@@ -25,15 +25,11 @@ func TestNewFilter(t *testing.T) {
 			},
 		},
 	}
-	var text = "这是一个阳光大男孩"
+	var text = "我是一个阳光开朗大男孩，兴趣爱好有打羽毛球，听女声毒物音乐"
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			filterManager := NewFilter()
-
-			//if err := filterManager.LoadDictPath("./dict/test_dict.txt"); err != nil {
-			//	t.Errorf("读入失败:%v", err)
-			//}
 
 			isSensitive := filterManager.IsSensitive(text)
 			t.Logf("IsSensitive= %v", isSensitive)
@@ -41,6 +37,16 @@ func TestNewFilter(t *testing.T) {
 			matchedAll := filterManager.FindAll(text)
 			t.Logf("所有敏感词为：%v", matchedAll)
 
+			replaceResult := filterManager.Replace(text, '*')
+			t.Logf("替换结果为:%v", replaceResult)
+
+			//读入新的dict
+			if err := filterManager.LoadDictPath("./dict/test_dict.txt"); err != nil {
+				t.Errorf("读入失败:%v", err)
+			}
+
+			matchedAllAfterLoad := filterManager.FindAll(text)
+			t.Logf("所有敏感词为：%v", matchedAllAfterLoad)
 		})
 	}
 }
